ae6596f51824f6ba33fbeaa170478058a34adb00
leratoMalebo <ato.malebo@gmail.com>
2023-08-02 11:06:17 +0200

    modified 3 files

--- db.js
+++ db.js
@@ -1,59 +1,87 @@
-import * as sqlite from 'sqlite' ;
-import  sqlite3 from 'sqlite3';
-
-
-
-const db=await sqlite.open({
-    filename:'./101.db',
+import * as sqlite from 'sqlite';
+import sqlite3 from 'sqlite3';
+​
+const db = await sqlite.open({
+    filename: './101.db',
     driver: sqlite3.Database
 });
-
-await db.migrate();
-
-//call the query sing an await
-// create a function that returns all the greetings from the db
-
- export async function getGreetings(){
-    const results =await db.all(`select * from greeting`);
-    return results;
-
-
+​
+​
+await db.migrate()
+​
+​
+​
+export async function getGreetings(){
+    const result = await db.all(`select * from greetings`);
+    return result   
 }
-
-
-//create a function that delete a specific greeting
-//create a function that adds a new greeting
-
-export async function addGreeting(language, greeting){
-
-     //sql statement type - insert
-
-    const sql =`insert into greeting(language, greeting) values (?,?)`
-    await db.run(sql, [language, greeting]);
-
+​
+// sync
+// const result = await getGreetings()
+// console.log(result)
+​
+​
+// async
+// getGreetings()
+// .then(result => {
+​
+//     console.log(result)
+// });
+​
+​
+// const count_results = await db.all(`select count(*) as count from greetings`);
+// console.log(count_results);
+​
+​
+// add greetng to the table
+export async function addGreeting(langage, greeting){
+​
+    //sql statement type - insert
+    const sql = `insert into greetings (langage, greeting) values (?, ?)`
+​
+    await db.run(sql, [langage, greeting]);
+​
 }
-
- export async function deleteGreeting(id){
-    //
-    const sql = `delete from greeting where id = ?`; 
-     await db.run(sql, [id]) 
+​
+//delete by id
+​
+export async function deleteGreeting(id){
+    const sql = `delete from greetings where id = ?`
+    await db.run(sql, [id])
+}   
+​
+// delete langage by langage
+export async function deleteGreetingByLangage(langage){
+    const sql = `delete from greetings where langage = ? `
+    await db.run(sql, [langage])
 }
-
- export async function updateGreeting(language, greeting, id){
-    const sql = `update greeting set language =?, greeting = ? where id =?`;
-    await db.run(sql, [language, greeting, id])
+​
+// do update the langage
+export async function updateLangage(langage, greeting, id){
+    const sql = `update greetings set langage= ?, greeting=? where id= ?`
+    await db.run(sql, [langage, greeting, id])
 }
-
-
-    const result1 = await getGreetings()
-   console.log(result1);
-
-
-    console.log('==============================================')
-
-    await addGreeting('Sepedi' , 'Thobela')
-
-    console.log('==============================================')
-
-    const result2 = await getGreetings()
-    console.log(result2);
+​
+const result1 = await getGreetings()
+console.log(result1)
+console.log("===========================================")
+​
+​
+// add langage
+// await addGreeting('venda', 'Ndaa');
+​
+// delete by id
+// await deleteGreeting(8);
+​
+// call delete by langage
+// await deleteGreetingByLangage('english')
+​
+// pdate langage
+​
+​
+// await updateLangage('venda', 'Aahh', '14')
+​
+console.log("===========================================")
+​
+const result2 = await getGreetings()
+console.log(result2)
--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 import express from 'express';
-import {getGreetings, addGreeting} from './db.js'
-
-const app = express();
-
-
-//http://localhost:4006/api/greetings
-
-//get all the greetings
-
-app.get('/api/greetings', async (req, res) =>{
-const greetings= await getGreeting();
-console.log(greetings);
-res.json({
-    greetings
-   })
-});
-
-//create a  route to add a greeting
-//?
-
-app.post('/api/greeting', (req, res)=>{
-
- const greeting=req.body.greeting;
- const language=req.body.language;
-
- res.json({
-    language,
-    greeting
- }
-
- )
+import { addGreeting, getGreetings, deleteGreeting, deleteGreetingByLangage, updateLangage } from './db.js';
+const app = express()
+//
+app.use(express.json());
+app.use(express.static('public'));
+const PORT = process.env.PORT || 4008;
+app.listen(PORT, ()=> console.log(`started on : ${PORT}`))
+// create the greetings api
+//get greetngs
+app.get('/api/greetings', async (req, res)=> {
+    const greetings = await getGreetings();
+    console.log(greetings)
+    res.json({
+        greetings
+    })
 })
-
-const PORT = process.env.PORT || 4006;
-app.listen(PORT, () => console.log(`started on port : ${PORT}`) )
-//ends here
+// riwu
+// add greetngs
+app.post('/api/greetings', async (req, res)=> {
+    const langage = req.body.langage
+    const greeting = req.body.greeting
+    // add langage
+    await addGreeting(langage, greeting);
+    res.json({
+        status: 'Success',
+        message: `greeting '${greeting}' added for ${langage}`
+        // langage, 
+        // greeting
+    })
+})
+// endpont for update
+app.post('/api/greetings/edt', async (req, res)=> {
+    const langage = req.body.langage
+    const greeting = req.body.greeting
+    // add langage
+    await addGreeting(langage, greeting);
+    res.json({
+        status: 'Success',
+        message: `greeting '${greeting}' added for ${langage}`
+        // langage, 
+        // greeting
+    })  
+})
+// console.log('start')
+// console.log('end')
